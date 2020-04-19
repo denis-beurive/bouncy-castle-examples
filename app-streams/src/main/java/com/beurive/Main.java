3b4e9f3529bcb4705c6175ad06d883a96eae9769
Denis BEURIVE <denis.beurive@synlab-consultants.com>
2020-04-19 15:33:20 +0200

    Add stream examples

--- app-streams/src/main/java/com/beurive/Main.java
+++ app-streams/src/main/java/com/beurive/Main.java
@@ -1,8 +1,25 @@
 package com.beurive;
 
+import java.io.*;
 
 
 public class Main {
+
+    // InputStream (java.io)
+    //    FileInputStream (java.io)
+    //        PipeInputStream in Process (java.lang)
+    //        SocketInputStream (java.net)
+    //        WDropTargetContextPeerFileStream (sun.awt.windows)
+
+    static private void showFileInputStream() throws FileNotFoundException, IOException {
+        FileInputStream stream;
+        String filePath = "./data/text-file.txt";
+
+        byte[] buffer = new byte[10];
+        stream = new FileInputStream(filePath);
+        stream.read(buffer);
+        stream.close();
+    }
 
     public static void main(String[] args) {
     }
